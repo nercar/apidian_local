f115d6799f4e6214d87f6d6147d66738a1d4b2df
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-10-09 17:25:33 -0400

    validando demonio 6

--- demmon/rev_DocEleEnv2Dian.php
+++ demmon/rev_DocEleEnv2Dian.php
@@ -108,8 +108,8 @@ if ($argc >= 1) {
                                 exit;
                             }
                         } else {
-                            echo $row['prefijo'] . ' ' . $row['folio'], "\r\n";
                             $cmd = "php renv_DocEleRev2API.php $instan $iptienda " . $row['prefijo'] . ' ' . $row['folio'];
+                            echo $cmd;
                             echo exec($cmd);
                         }
                     }
