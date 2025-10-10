e99183c9eaef1cfe92b5f3d0be5d40c710a67a9d
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-10-10 10:16:24 -0400

    adsfa

--- demmon/renv_DocEleRev2API.php
+++ demmon/renv_DocEleRev2API.php
@@ -333,7 +333,7 @@ function getData($conSQLLoc, $tienda, $cnx, $number, $prefix, $newtype = 0)
 function envJson2Api($conSQLLoc, $tienda, $cnx, $number, $prefix, $jsObj, $type_document_id, $identification_number)
 {
     $curl = curl_init();
-    $url  = "http://localhost/api/ubl2.1/";
+    $url  = "http://localhost/apidian/public/api/ubl2.1/";
     $url .= ($type_document_id == 1) ? 'invoice' : 'credit-note';
     echo " Enviando [$prefix-$number] -> ";
     curl_setopt_array($curl, [
@@ -405,7 +405,7 @@ function envJson2Api($conSQLLoc, $tienda, $cnx, $number, $prefix, $jsObj, $type_
                         AND id = (SELECT MAX(ID) FROM documents
                         WHERE prefix = '$prefix' AND number = '$number')");
             if ($identification_number != "222222222222") {
-                $url  = "http://localhost/api/ubl2.1/send-email";
+                $url  = "http://localhost/apidian/public/api/ubl2.1/send-email";
                 echo " Mail [$prefix-$number] -> ";
                 $jsObj = array(
                     "prefix" => "$prefix",
