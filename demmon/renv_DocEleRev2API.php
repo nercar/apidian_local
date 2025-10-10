78be33dedb0266e01a538409509269264dbee8b5
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-10-10 10:47:04 -0400

    asdA

--- demmon/renv_DocEleRev2API.php
+++ demmon/renv_DocEleRev2API.php
@@ -325,7 +325,7 @@ function getData($conSQLLoc, $tienda, $cnx, $number, $prefix, $newtype = 0)
                 $jsObj->legal_monetary_totals->payable_amount = round($jsObj->legal_monetary_totals->tax_inclusive_amount + $recargo, 2);
             }
             // envJson2Api($conSQLLoc, $tienda, $cnx, $number, $prefix, $jsObj, $row['type_document_id'], $row['identification_number']);
-            echo json_encode($jsObj);
+            print_r($jsObj);
             // echo "\r\n";
         }
     }
