9cbc375a026cab76eda48356d719e506bb4eea9b
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-10-10 11:28:35 -0400

    asda

--- demmon/renv_DocEleRev2API.php
+++ demmon/renv_DocEleRev2API.php
@@ -325,7 +325,7 @@ function getData($conSQLLoc, $tienda, $cnx, $number, $prefix, $newtype = 0)
                 $jsObj->legal_monetary_totals->payable_amount = round($jsObj->legal_monetary_totals->tax_inclusive_amount + $recargo, 2);
             }
             // envJson2Api($conSQLLoc, $tienda, $cnx, $number, $prefix, $jsObj, $row['type_document_id'], $row['identification_number']);
-            print_r((array)$jsObj);
+            echo json_encode($jsObj);
             // echo "\r\n";
         }
     }
