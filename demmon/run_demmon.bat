ab9a4335aa8f31271ab3f2d31bc2ea57cd3b286c
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-11-07 09:15:29 -0400

    modificacion bateria

--- demmon/run_demmon.bat
+++ demmon/run_demmon.bat
@@ -1,4 +1,8 @@
+:: Script para ejecutar demonios o script php
+:: Parametro 1 debe ser el nombre del archivo .php que se va a ejecutar
+:: Parametros 2 - 5 son opcionales se indican si son necesarios
+:: Ejemplo run_demmon.bat env_DocElecPend2API n.a localhost
 @echo off
 set demmon=%1
 set logfile=%date:~0,3%.log
-php %demmon%.php n.a localhost >> %demmon%_%logfile%
+php %demmon%.php %2 %3 %4 %5 >> %demmon%_%logfile%
