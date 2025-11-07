0bc00f4fa62e459d3451313ead662f00b53da3f1
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-11-07 09:44:08 -0400

    Eliminados demonios innecesarios

--- demmon/limpiar_logs.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dias = array("domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado");
-$execstring = "cd /var/www/html/apidian/demmon; find log_*_*_" . $dias[date('w') + 1] . ".log -type f -exec rm -f {} \;";
-$output = [];
-echo exec($execstring, $output);
--- demmon/rev_DocEleEnv2Dian.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set demon=rev_DocEleEnv2Dian
-set logfile=%date:~0,3%.log
-php %demon%.php n.a localhost >> %demon%_%logfile%
