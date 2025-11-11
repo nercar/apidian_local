21d42ddbc91ab093328e25073623f9719ca4e17d
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-11-11 11:58:53 -0400

    Fix get day from date

--- demmon/run_demmon.bat
+++ demmon/run_demmon.bat
@@ -4,9 +4,14 @@
 ::              Parametro          %1         %2     %3
 :: Ejemplo run_demmon.bat env_DocElecPend2API n.a localhost
 @echo off
+setlocal 
 set demmon=%1
-set logfile=%date:~0,3%.log
+for /f "skip=8 tokens=2,3,4,5,6,7,8 delims=: " %%D in ('robocopy /l * \ \ /ns /nc /ndl /nfl /np /njh /XF * /XD *') do (
+ set "dow=%%D"
+)
+set logfile=%dow:~0,3%.log
 :: Eliminar archivos log viejos 7 días antes del archivo que se ejecuta
 forfiles /m *%demmon%*.log /d -7 /c "cmd /c del /q @path"
 :: Ejecutar el php indicado con los parametros indicados
 php %demmon%.php %2 %3 %4 %5 >> %demmon%_%logfile%
+endlocal
