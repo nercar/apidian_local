242e874e3933adbcb5308ba9f186792f23597470
Carlos Ramirez <70415782+nercar@users.noreply.github.com>
2025-11-17 09:58:29 -0400

    debug

--- demmon/rev_DocEleEnv2Dian.php
+++ demmon/rev_DocEleEnv2Dian.php
@@ -108,7 +108,8 @@ if ($argc >= 1) {
                         } else {
                             if (PHP_OS == 'Linux') {
                                 $cmd = "php /var/www/html/apidian/demmon/renv_DocEleRev2API.php $instan $iptienda " . $row['prefijo'] . ' ' . $row['folio'];
-                                echo exec($cmd);
+                                echo $cmd, "\r\n";
+                                // echo exec($cmd);
                             } else {
                                 $dir = __DIR__ . DIRECTORY_SEPARATOR;
                                 $cmd = "php " . $dir . "renv_DocEleRev2API.php $instan $iptienda " . $row['prefijo'] . ' ' . $row['folio'];
